a4a55caf9a605c5897b94f0a89308444a0a69358
JeremyNeuberger <Jeremy@TravelTab.com>
2018-07-12 06:52:05 -0400

    changing classes to Fluxx, remove Store annotation, adding FluxxView interface to force registration

--- src/main/java/com/ptmr3/fluxx/FluxxReaction.kt
+++ src/main/java/com/ptmr3/fluxx/FluxxReaction.kt
@@ -2,7 +2,7 @@ package com.ptmr3.fluxx
 
 import android.support.v4.util.ArrayMap
 
-class FluxReaction internal constructor(val type: String, val data: ArrayMap<String, Any>?) {
+class FluxxReaction internal constructor(val type: String, val data: ArrayMap<String, Any>?) {
 
     operator fun <T> get(tag: String): T {
         return data!![tag] as T
@@ -18,17 +18,17 @@ class FluxReaction internal constructor(val type: String, val data: ArrayMap<Str
         if (this === other) {
             return true
         }
-        if (other !is FluxReaction) {
+        if (other !is FluxxReaction) {
             return false
         }
-        val fluxReaction = other as FluxReaction?
+        val fluxReaction = other as FluxxReaction?
         return if (type != fluxReaction!!.type) {
             false
         } else !if (data != null) data != fluxReaction.data else fluxReaction.data != null
     }
 
     override fun toString(): String {
-        return "FluxAction{" + "mType='" + type + '\''.toString() + ", mData=" + data + '}'.toString()
+        return "FluxxAction{" + "Type='" + type + '\''.toString() + ", Data=" + data + '}'.toString()
     }
 
     class Builder {
@@ -55,11 +55,11 @@ class FluxReaction internal constructor(val type: String, val data: ArrayMap<Str
             return this
         }
 
-        fun build(): FluxReaction {
+        fun build(): FluxxReaction {
             if (type == null || type!!.isEmpty()) {
                 throw IllegalArgumentException("At least one key is required.")
             }
-            return FluxReaction(type!!, data)
+            return FluxxReaction(type!!, data)
         }
     }
 
---
+++ src/main/java/com/ptmr3/fluxx/FluxxView.kt
@@ -0,0 +1,5 @@
+package com.ptmr3.fluxx
+
+interface FluxxView {
+    fun registerStore()
+}
